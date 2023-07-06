3c9d2f7dc51d97847dcad6c680b51c0ebc615e27
Zack Jones <zajonesck83@gmail.com>
2023-07-06 15:56:20 -0500

    fixed nav bar link

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,8 +48,8 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+// modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+// overlay.addEventListener("click", testimonialsModalFunc);
 
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
